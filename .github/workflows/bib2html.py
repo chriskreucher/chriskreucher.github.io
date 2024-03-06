82ec3ecb3d632fdca19cc5e6adb33078ecd3421c
chriskreucher <162367330+chriskreucher@users.noreply.github.com>
2024-03-06 09:45:04 -0500

    Update and rename example2.py to bib2html.py

--- .github/workflows/bib2html.py
+++ .github/workflows/bib2html.py
@@ -10,20 +10,13 @@ import re
 #todo: incorporate different collection types rather than a catch all publications, requires other changes to template
 publist = {
     "proceeding": {
-        "file" : "proceedings.bib",
-        "venuekey": "booktitle",
+        "file" : "Kreucher.bib",
+        "venuekey": "article",
         "venue-pretext": "In the proceedings of ",
         "collection" : {"name":"publications",
                         "permalink":"/publication/"}
         
-    },
-    "journal":{
-        "file": "pubs.bib",
-        "venuekey" : "journal",
-        "venue-pretext" : "",
-        "collection" : {"name":"publications",
-                        "permalink":"/publication/"}
-    } 
+    }
 }
 
 html_escape_table = {
