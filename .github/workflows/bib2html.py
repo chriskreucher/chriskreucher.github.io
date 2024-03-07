390f08fe3bf92c7071df9776b793987311f4561c
chriskreucher <162367330+chriskreucher@users.noreply.github.com>
2024-03-07 08:48:44 -0500

    Update bib2html.py

--- .github/workflows/bib2html.py
+++ .github/workflows/bib2html.py
@@ -270,7 +270,8 @@ def main():
 
     # write the entire bibliography
     #fname_html = './_pages/complete-bibliography.md'
-    fname_html = '../_publications/' + os.path.basename('complete-bibliography.md')
+    #fname_html = '../_publications/' + os.path.basename('complete-bibliography.md')
+    fname_html = '_publications/complete-bibliography.md'
     print("\n Writing " + str(fname_html) )
     all_html = open(fname_html,'w')
     populate_header(all_html, unique_years, unique_types)
