2774fd59a6feff852325818fe3f6b579eab23412
chriskreucher <162367330+chriskreucher@users.noreply.github.com>
2024-03-08 13:32:35 -0500

    x

--- markdown_generator/bib2html.py
+++ markdown_generator/bib2html.py
@@ -277,7 +277,7 @@ def main():
             fname = '_publications/publication-' + str(i) + '.md'
             pub = open( fname ,'w')
             pub.write('---')
-            pub.write('\npaper_title :"' + titles[i] + '"' )
+            pub.write('\npaper_title : "' + titles[i] + '"' )
             pub.write('\npdf_link : "' + pdfs[i] + '"' )
             pub.write('\nwww_link : "' + wwws[i] + '"' )
             pub.write('\n---')
